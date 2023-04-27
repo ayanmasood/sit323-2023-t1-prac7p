32bec195761ccf40326dd5c8e98d348f5e30b256
ayanmasood <87452834+ayanmasood@users.noreply.github.com>
2023-04-27 13:45:22 +1000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express")
+var app = express()
+
+app.get('/', function(req,res){
+    
+})
+
+
+app.listen(3040, function (request, response){
+    console.log("server on 3040")
+})
